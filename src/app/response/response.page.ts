7c19697aca4893275c686d3dd6e1882cf664e679
sergi <sergi@unievegroup.com>
2019-10-21 15:51:44 +0200

    Added response page

---
+++ src/app/response/response.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-response',
+  templateUrl: './response.page.html',
+  styleUrls: ['./response.page.scss'],
+})
+export class ResponsePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
